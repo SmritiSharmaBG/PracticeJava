649f3f89b613f46d92320f8801285ed61c7480a4
Smriti Sharma <77639024+SmritiSharmaBG@users.noreply.github.com>
2023-12-17 15:05:59 +0530

    Add files via upload

---
+++ Immutability/Employee.java
@@ -0,0 +1,77 @@
+package Core;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+public final class Employee {
+    private final String name;
+    private final Date DOJ;
+    private final ArrayList<Integer> mobileNumbers;
+    private final Address address;
+
+    public Employee(String name, Date DOJ, ArrayList<Integer> mobileNumbers, Address address) {
+        this.name = name;
+        this.DOJ = DOJ;
+        this.mobileNumbers = mobileNumbers;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getDOJ() {
+//        return DOJ;
+        return (Date) DOJ.clone();
+    }
+
+    public ArrayList<Integer> getMobileNumbers() {
+        return (ArrayList<Integer>) mobileNumbers.clone();
+    }
+
+    public Address getAddress() {
+        return new Address(address.getCity(), address.getPinCode());
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", DOJ=" + DOJ +
+                ", mobileNumbers=" + mobileNumbers +
+                ", address=" + address +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Employee employee = new Employee("Smriti",
+                new Date(2020, 11, 28),
+                new ArrayList<>(Arrays.asList(1234, 987)),
+                new Address("Ahemdabad", 2233));
+
+        // employee.name = "Shruti"; -> COMPILATION ERROR
+        // employee.DOJ = new Date(2021, 01, 01); -> COMPILATION ERROR because we are directly changing the final parameter
+
+        // However, changing the DOJ via the function will change the DOJ even though its final!
+        employee.getDOJ().setDate(18); // this will change the date
+        // to avoid this, return a clone of object reference.
+
+        // this can change the list.
+        // employee.getMobileNumbers().add(55);
+        // So we return a clone of it
+        employee.getMobileNumbers().add(55);
+
+        // this will change the City.
+        // employee.getAddress().setCity("Delhi");
+        // So we return a deep copy of it
+        employee.getAddress().setCity("Delhi");
+
+        // But,
+        employee.mobileNumbers.add(66);
+        // it will still change the number because we are accessing the list directly.
+        // in real-world, these types of functions are present outside the class and such activities can't be done, thanks to PRIVATE access-modifier!
+
+        System.out.println(employee);
+    }
+}
---
+++ Immutability/Immutability.md
@@ -0,0 +1,11 @@
+## Immutability
+
+#### How can you make your own Immutable class?
+
+1. Declare the class as final : no other class and inherit it.
+2. All the variables must be private : no other class and acecss them.
+3. All the variables must be final : no function can modify them. 
+   - So, we will need to get it initialised in the constructor.
+4. No setters, only getters (generating setter will give compile time error anyway).
+5. In the getter of the object reference, return a clone instead of returning it directly. Doing this, the getter return a clone of the parameter (and keeps the actual parameter safe) with which the client can use it without altering its actual value.
+6. For mutable Objects, we will need to return a deep copy.
